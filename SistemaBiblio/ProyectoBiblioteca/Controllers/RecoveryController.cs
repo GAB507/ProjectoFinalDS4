58c978807d0e1246fea84dde29cb6fd5820c68db
Gabriel Cubilla <gabriel.cubilla@utp.ac.pa>
2024-12-02 19:14:15 -0500

    update final project DS4

---
+++ SistemaBiblio/ProyectoBiblioteca/Controllers/RecoveryController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoBiblioteca.Controllers
+{
+    public class RecoveryController : Controller
+    {
+        // GET: Recovery
+        public ActionResult RecuperarPassword()
+        {
+            return View();
+        }
+         
+     }
+}
